39c448fe6ffe48dbf45e9c4fe5ff71c5796a60cc
Kushagra Vashisht <vashishtkushagra@gmail.com>
2020-12-25 22:21:58 +1100

    Automating the VM creation along with Ubuntu installation working

---
+++ create_vm_Linux_64_git.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Creating a VM and registering it 
+VBoxManage createvm --name Test1_Linux_VM --ostype Ubuntu_64 --register
+
+# Setting systembase memory to the VM (2048MB), RAM (8GB) and VirtualCPUs (2).
+# Setting Shared Clipboard and Drag n Drop to bidirectional
+# Disabling Audio, USB, USB2.0, USB3.0 since there's no need for them
+VBoxManage modifyvm Test1_Linux_VM --memory 2048 --vram 8 --cpus 2 --audio none --usb off --usbehci off --usbxhci off --clipboard-mode bidirectional --draganddrop bidirectional
+
+# Create Virtual Hard Disk of size 10GB (recommended) and of format VDI
+VBoxManage createhd --filename VirtualBox\ VMs/Test1_Linux_VM/Test1_Linux_VM.vdi --size 10000 --format VDI
+
+# Creating a SATA controller which we will attach the virtual disk to
+VBoxManage storagectl Test1_Linux_VM --name "SATA Controller" --add sata --controller IntelAhci
+
+# Attaching the virtual disk to the SATA controller
+VBoxManage storageattach Test1_Linux_VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium VirtualBox\ VMs/Test1_Linux_VM/Test1_Linux_VM.vdi
+
+# Adding the IDE controller since we're creating a fresh VM from ISO image
+VBoxManage storagectl Test1_Linux_VM --name "IDE Controller" --add ide --controller PIIX4
+
+# Attach the ISO image to IDE controller (Ubuntu 20.04.1)
+VBoxManage storageattach Test1_Linux_VM --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium <Full path to the location of the ISO file>
+
+# Unattended installation and start VM
+VBoxManage unattended install Test1_Linux_VM --user=<username> --password=<password, without quotes> --country=AU --time-zone=AEDT --iso=<Full path to the location of the ISO file> --start-vm=gui
+
+# Starting the VM (No Need for this bit, still testing out things)
+# VBoxManage startvm Test1_Linux_VM
---
+++ deletevm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Delete VM Test1_Linux_VM
+VBoxManage unregistervm Test1_Linux_VM --delete
