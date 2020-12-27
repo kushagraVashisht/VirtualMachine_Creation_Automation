eeec33f4ccdae725fabb4861624c062c45fd69f5
Kushagra Vashisht <vashishtkushagra@gmail.com>
2020-12-27 21:53:41 +1100

    Updating changes

--- create_vm_Linux_64_git.sh
+++ create_vm_Linux_64_git.sh
@@ -20,7 +20,7 @@ VBoxManage createvm --name Test1_Linux_VM --ostype Ubuntu_64 --register
 # Setting systembase memory to the VM (2048MB), RAM (8GB) and VirtualCPUs (2).
 # Setting Shared Clipboard and Drag n Drop to bidirectional
 # Disabling Audio, USB, USB2.0, USB3.0 since there's no need for them
-VBoxManage modifyvm Test1_Linux_VM --memory 2048 --vram 8 --cpus 2 --audio none --usb off --usbehci off --usbxhci off --clipboard-mode bidirectional --draganddrop bidirectional
+VBoxManage modifyvm Test1_Linux_VM --memory 2048 --vram 256 --cpus 2 --audio none --usb off --usbehci off --usbxhci off --clipboard-mode bidirectional --draganddrop bidirectional
 
 # Create Virtual Hard Disk of size 10GB (recommended) and of format VDI
 VBoxManage createhd --filename VirtualBox\ VMs/Test1_Linux_VM/Test1_Linux_VM.vdi --size 10000 --format VDI
@@ -38,7 +38,7 @@ VBoxManage storagectl Test1_Linux_VM --name "IDE Controller" --add ide --control
 VBoxManage storageattach Test1_Linux_VM --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium <Full path to the location of the ISO file>
 
 # Unattended installation and start VM
-VBoxManage unattended install Test1_Linux_VM --user=<username> --password=<password, without quotes> --country=AU --time-zone=AEDT --iso=<Full path to the location of the ISO file> --start-vm=gui
+VBoxManage unattended install Test1_Linux_VM --user=kush --password=Sample@123 --country=AU --time-zone=AEDT --iso=<full path of the ISO file> --post-install-template=<Full path of the post install bash script> --start-vm=gui
 
 # Starting the VM (No Need for this bit, still testing out things)
 # VBoxManage startvm Test1_Linux_VM
