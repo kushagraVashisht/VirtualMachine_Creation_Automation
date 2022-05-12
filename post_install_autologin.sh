cf8e0caac6566027abbcae9fa0c9dcc3599bf59e
Kushagra Vashisht <vashishtkushagra@gmail.com>
2022-05-12 16:31:23 +0530

    Added the logic to run the post_install_script at startup and get rid of the bugs related to terminal not being accessible

--- post_install_autologin.sh
+++ post_install_autologin.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Get root access
+su root 
+
+# Change the language settings to get rid of the bugs
+sed -i -e 's/LANG=en_US/LANG=en_US.UTF-8/g' /etc/default/locale
+
 # Replacing the contents of the /etc/gdm3/custom.conf file to enable autologin since this cannot be set during unattended install
-sudo sed -i.bu -e 's/# AutomaticLoginEnable = true/AutomaticLoginEnable = true/' -e 's/# AutomaticLogin = user1/AutomaticLogin = kush/' /etc/gdm3/custom.conf
+# sed -i.bu -e 's/# AutomaticLoginEnable = true/AutomaticLoginEnable = true/' -e 's/# AutomaticLogin = user1/AutomaticLogin = kush/' /etc/gdm3/custom.conf
 
 # Restart 
-sudo reboot
+reboot
